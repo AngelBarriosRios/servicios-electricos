c58de2d3944c80528f252141d467d0d3bf395f8d
Angel Antonio Barrios Rios <jacktdyret0109@gmail.com>
2026-02-27 14:19:05 -0600

    Update emailService.js

--- utils/emailService.js
+++ utils/emailService.js
@@ -7,8 +7,8 @@ dns.setDefaultResultOrder('ipv4first'); // <--- LÍNEA CRÍTICA
 // Configurar transporte de email con opciones específicas
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 587,
-    secure: false, // true para 465, false para 587
+    port: 465,
+    secure: true, // true para 465, false para 587
     requireTLS: true,
     auth: {
         user: process.env.EMAIL_USER || 'tu-email@gmail.com',
